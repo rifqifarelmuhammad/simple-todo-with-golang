5b057c5a86388a2458e1e4f0ec7b28c2e6c27e69
rifqifarelmuhammad <rifqifarel222@gmail.com>
2023-08-21 23:35:42 +0700

    Feat: Database Indexes

--- internal/models/todo.go
+++ internal/models/todo.go
@@ -1,11 +1,11 @@
 package models
 
 type Todo struct {
-	ID          string `gorm:"primaryKey" json:"id"`
+	ID          string `gorm:"primaryKey;index" json:"id"`
 	Title       string `json:"title"`
 	Description string `json:"description"`
 	IsCompleted bool   `gorm:"default:false" json:"is_completed"`
 	Timestamp
-	IsDeleted bool `gorm:"default:false" json:"is_deleted"`
-	UserID    string
+	IsDeleted bool   `gorm:"default:false" json:"is_deleted"`
+	UserID    string `gorm:"index"`
 }
--- internal/models/user.go
+++ internal/models/user.go
@@ -1,8 +1,8 @@
 package models
 
 type User struct {
-	UID      string `gorm:"primaryKey;column:id" json:"id"`
-	Email    string `gorm:"unique" json:"email"`
+	UID      string `gorm:"primaryKey;column:id;index" json:"id"`
+	Email    string `gorm:"unique;index" json:"email"`
 	Password string `json:"password"`
 	Todos    []Todo `gorm:"foreignKey:UserID"`
 	Timestamp
